261e984f133e16af3f5131ae9a3c3f7d5628c606
Ninochakvetadze <nino.chakvetadze28b@geolab.edu.ge>
2023-04-20 14:37:09 -0700

    md

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./css/index.css";
-import { newMap } from "./pages";
+import { newMap, Header, Footer } from "./pages";
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <Header />
         <BrowserRouter>
             <Routes>
                 {newMap.map((route, index) => (
@@ -19,6 +20,7 @@ root.render(
                 ))}
             </Routes>
         </BrowserRouter>
+        <Footer />
     </React.StrictMode>
 );
 
